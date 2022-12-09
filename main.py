bb26bd8b80c454c857d3881e4a816b8ac1b8b676
YouPiterOn <oyushchenko@kse.org.ua>
2022-12-09 16:43:30 +0200

    third task

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Participant:
         self.medal = medal
 
 
+# task 1
 if sys.argv[2] == "-medals":
     with open(sys.argv[1], 'r') as file:
         line = file.readline()
@@ -31,3 +32,20 @@ if sys.argv[2] == "-medals":
             if (participant.team == sys.argv[3] or participant.noc == sys.argv[3]) and participant.year == sys.argv[4] and participant.medal != "NA":
                 print(participant.name, participant.sport, participant.medal)
                 counter += 1
+
+# task 3
+if sys.argv[2] == "-overall":
+    with open(sys.argv[1], 'r') as file:
+        line = file.readline()
+        years = {}
+        for i in range(1896, 2018, 2):
+            years[i] = 0
+        for country in sys.argv[3::]:
+            for line in file.readlines():
+                line = line[0:-1]
+                participant = Participant(*line.split("\t"))
+                if participant.team == country or participant.noc == country and participant.medal != "NA":
+                    years[int(participant.year)] += 1
+            print(max(years, key=years.get), max(years.values()))
+            for i in range(1896, 2020, 4):
+                years[i] = 0

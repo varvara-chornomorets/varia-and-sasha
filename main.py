d7cc4fd0483b9d5397d6fcdd2616159b4722bf07
Varvara Chornomorets <vchornomorets@kse.org.ua>
2022-12-09 14:19:06 +0200

    moved class to a separate file, almost done 1st task using argparse

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 import sys
-
-
-class Participant:
-
-    def __init__(self, id, name, sex, age, height, weight, team, noc, games, year, season, city, sport, event, medal):
-        self.id = id
-        self.name = name
-        self.sex = sex
-        self.age = age
-        self.height = height
-        self.weight = weight
-        self.team = team
-        self.noc = noc
-        self.games = games
-        self.year = year
-        self.season = season
-        self.city = city
-        self.sport = sport
-        self.event = event
-        self.medal = medal
+from cls import Participant
 
 
 if sys.argv[2] == "-medals":
--- main_argparse.py
+++ main_argparse.py
@@ -1,42 +1,44 @@
 import argparse
+from cls import Participant
 
 types_of_medals = ["Gold", "Silver", "Bronze"]
-# def myFunc():
-#     return
 
 
-def count_number_of_medals(type_of_medals, occurrences):
+def count_number_of_medals(type_of_medals, list_of_participants):
     number = 0
-    for i in range(0, len(occurrences)):
-        if type_of_medals in occurrences[i]:
+    for i in range(0, len(list_of_participants)):
+        if list_of_participants[i].medal == type_of_medals:
             number += 1
     return number
 
 
 def task1(filename, country, year):
-    occurrences = []
+    suitable_participants = []
     with open(filename, "r") as file:
         first_line = file.readline()
         head = first_line.strip().split("\t")
         for line in file.readlines():
-            data = line.strip().split("\t")
-            if country == data[head.index("Team")] and year == data[head.index("Year")]:
-                occurrences.append(data)
+            participant = Participant(*line.strip().split("\t"))
+            if (participant.team == country or participant.noc == country) and participant.medal != "NA" and participant.year == year:
+                suitable_participants.append(participant)
+
+        for i in range(0, 10):
+            participant = suitable_participants[i]
+            print(f"{participant.name}-{participant.sport}-{participant.medal}")
+
         for type_of_medal in types_of_medals:
-            k = count_number_of_medals(type_of_medal, occurrences)
+            k = count_number_of_medals(type_of_medal, suitable_participants)
             print(type_of_medal, k)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    # parser.add_argument("filename")
-    parser.add_argument("--medals", action="store_true", required=False)
+    parser.add_argument("filename")
+    parser.add_argument("-medals", action="store_true", required=False)
     parser.add_argument("country")
     parser.add_argument("year")
     args = parser.parse_args()
-    print(args)
-    # filename = args.filename
-    filename = "athlete_events"
+    filename = args.filename
     country = args.country
     year = args.year
     medals = args.medals

f838d6c6a030185b458e140601c51441478ed27c
YouPiterOn <oyushchenko@kse.org.ua>
2022-12-09 17:45:21 +0200

    output to file and fixes in first task

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ if sys.argv[2] == "-medals":
     with open(sys.argv[1], 'r') as file:
         line = file.readline()
         counter = 0
-        while counter < 10:
-            line = file.readline()[0:-1]
+        for line in file.readlines():
+            line = line[0:-1]
             participant = Participant(*line.split("\t"))
             if (participant.team == sys.argv[3] or participant.noc == sys.argv[3]) and participant.year == sys.argv[4] and participant.medal != "NA":
                 print(participant.name, participant.sport, participant.medal)
+                if sys.argv[5] == "-output":
+                    with open(sys.argv[6], 'a') as output_file:
+                        output_file.write(participant.name+', '+participant.sport+', '+participant.medal+"\n")
                 counter += 1
+                if counter == 10:
+                    break
 
 # task 3
 if sys.argv[2] == "-overall":

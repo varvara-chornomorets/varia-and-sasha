8b3c333bfac78078a87fc2bcadc85e9c1ede3af1
YouPiterOn <oyushchenko@kse.org.ua>
2022-12-09 19:29:10 +0200

    update of first task

--- main.py
+++ main.py
@@ -7,17 +7,32 @@ if sys.argv[2] == "-medals":
     with open(sys.argv[1], 'r') as file:
         line = file.readline()
         counter = 0
+        suitable_participants = []
         for line in file.readlines():
             line = line[0:-1]
             participant = Participant(*line.split("\t"))
             if (participant.team == sys.argv[3] or participant.noc == sys.argv[3]) and participant.year == sys.argv[4] and participant.medal != "NA":
                 print(participant.name, participant.sport, participant.medal)
-                if sys.argv[5] == "-output":
-                    with open(sys.argv[6], 'a') as output_file:
-                        output_file.write(participant.name+', '+participant.sport+', '+participant.medal+"\n")
+                if len(sys.argv) > 5:
+                    if sys.argv[5] == "-output":
+                        with open(sys.argv[6], 'a') as output_file:
+                            output_file.write(participant.name+', '+participant.sport+', '+participant.medal+"\n")
                 counter += 1
+                suitable_participants.append(participant)
                 if counter == 10:
                     break
+        if len(suitable_participants) > 0:
+            for type_of_medal in ["Gold", "Silver", "Bronze"]:
+                number = 0
+                for i in range(0, len(suitable_participants)):
+                    if suitable_participants[i].medal == type_of_medal:
+                        number += 1
+                print(type_of_medal, number)
+                if len(sys.argv) > 5:
+                    if sys.argv[5] == "-output":
+                        with open(sys.argv[6], 'a') as output_file:
+                            output_file.write(type_of_medal+" "+str(number)+"\n")
+
 
 # task 3
 if sys.argv[2] == "-overall":
